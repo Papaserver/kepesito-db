f999170158debecad90562d6e6da93d065e55a37
Sipkó Miklós <miklossipko@gmail.com>
2021-04-29 11:08:40 +0200

    All tests passed

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -12,4 +16,22 @@ public class JurassicPark {
         this.dbPassword = dbPassword;
     }
 
+    public List<String> checkOverpopulation() {
+        List<String> overpopulated = new ArrayList<>();
+        try (Connection con = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            String sql = "select breed from dinosaur " +
+                    "where expected < actual " +
+                    "order by breed";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                if (!overpopulated.contains(rs.getString("breed"))) {
+                    overpopulated.add(rs.getString("breed"));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return overpopulated;
+    }
 }
